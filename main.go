7b48ede1f847afc429b37c4a2ae404318ee15a89
hehaowen00 <edmundhe@outlook.com>
2022-07-10 14:53:04 +1000

    Store wasm execution environments for reuse

--- main.go
+++ main.go
@@ -1,175 +1,175 @@
 package main
 
 import (
-  "context"
-  "encoding/json"
-  "github.com/gorilla/mux"
-  "log"
-  "net"
-  "net/http"
-  "os"
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
 var configInstance *Config
 
 type Config struct {
-  Addr    string `json:"addr"`
-  Port    string `json:"port"`
-  Db      string `json:"db_path"`
-  Fs      string `json:"fs_dir"`
-  Modules string `json:"modules_dir"`
+	Addr    string `json:"addr"`
+	Port    string `json:"port"`
+	Db      string `json:"db_path"`
+	Fs      string `json:"fs_dir"`
+	Modules string `json:"modules_dir"`
 }
 
 func ConfigFromPathOrDefault(path string) (Config, error) {
-  var config Config
+	var config Config
 
-  if path == "" {
-    path = "./config.json"
-  }
+	if path == "" {
+		path = "./config.json"
+	}
 
-  if _, err := os.Stat(path); os.IsNotExist(err) {
-    config = Config{
-      Addr:    "127.0.0.1",
-      Port:    "8080",
-      Db:      "./wasm.db",
-      Fs:      "./fs",
-      Modules: "./modules",
-    }
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		config = Config{
+			Addr:    "127.0.0.1",
+			Port:    "8080",
+			Db:      "./wasm.db",
+			Fs:      "./fs",
+			Modules: "./modules",
+		}
 
-    bytes, err := json.MarshalIndent(&config, "", "  ")
-    if err != nil {
-      return config, err
-    }
+		bytes, err := json.MarshalIndent(&config, "", "  ")
+		if err != nil {
+			return config, err
+		}
 
-    f, err := os.Create(path)
-    if err != nil {
-      return config, err
-    }
-    defer f.Close()
+		f, err := os.Create(path)
+		if err != nil {
+			return config, err
+		}
+		defer f.Close()
 
-    _, err = f.WriteString(string(bytes))
+		_, err = f.WriteString(string(bytes))
 
-    return config, err
-  }
+		return config, err
+	}
 
-  bytes, err := os.ReadFile(path)
-  if err != nil {
-    return config, err
-  }
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return config, err
+	}
 
-  err = json.Unmarshal(bytes, &config)
+	err = json.Unmarshal(bytes, &config)
 
-  return config, err
+	return config, err
 }
 
 func (c *Config) MakeDirs() error {
-  if _, err := os.Stat(c.Fs); os.IsNotExist(err) {
-    err := os.Mkdir(c.Fs, 0777)
-    if err != nil {
-      return err
-    }
-  }
+	if _, err := os.Stat(c.Fs); os.IsNotExist(err) {
+		err := os.Mkdir(c.Fs, 0777)
+		if err != nil {
+			return err
+		}
+	}
 
-  if _, err := os.Stat(c.Modules); os.IsNotExist(err) {
-    err := os.Mkdir(c.Modules, 0777)
-    if err != nil {
-      return err
-    }
-  }
+	if _, err := os.Stat(c.Modules); os.IsNotExist(err) {
+		err := os.Mkdir(c.Modules, 0777)
+		if err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func (c *Config) FsDir() string {
-  return c.Fs
+	return c.Fs
 }
 
 func (c *Config) ModulesDir() string {
-  return c.Modules
+	return c.Modules
 }
 
 type FunctionConfig struct {
-  Name    string            `json:"name"`
-  Timeout int64             `json:"timeout"`
-  Methods []string          `json:"methods"`
-  Folders []string          `json:"folders"`
-  Vars    map[string]string `json:"vars"`
+	Name    string            `json:"name"`
+	Timeout int64             `json:"timeout"`
+	Methods []string          `json:"methods"`
+	Folders []string          `json:"folders"`
+	Vars    map[string]string `json:"vars"`
 }
 
 func (config *FunctionConfig) parse(data string) error {
-  err := json.Unmarshal([]byte(data), config)
-  if err != nil {
-    return err
-  }
-  return nil
+	err := json.Unmarshal([]byte(data), config)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (config *FunctionConfig) blob() ConfigBlob {
-  return ConfigBlob{
-    Folders: config.Folders,
-    Vars:    config.Vars,
-  }
+	return ConfigBlob{
+		Folders: config.Folders,
+		Vars:    config.Vars,
+	}
 }
 
 type ConfigBlob struct {
-  timeout int64
-  Folders []string          `json:"folders"`
-  Vars    map[string]string `json:"vars"`
+	timeout int64
+	Folders []string          `json:"folders"`
+	Vars    map[string]string `json:"vars"`
 }
 
 type Key struct {
-  key string
+	key string
 }
 
 var ContextKey = &Key{"connection"}
 
-func SaveConnection(ctx context.Context, c net.Conn) (context.Context) {
-  return context.WithValue(ctx, ContextKey, c)
+func SaveConnection(ctx context.Context, c net.Conn) context.Context {
+	return context.WithValue(ctx, ContextKey, c)
 }
-func GetConn(r *http.Request) (net.Conn) {
-  return r.Context().Value(ContextKey).(net.Conn)
+func GetConn(r *http.Request) net.Conn {
+	return r.Context().Value(ContextKey).(net.Conn)
 }
 
 func main() {
-  log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-  config, err := ConfigFromPathOrDefault("config.json")
-  if err != nil {
-    log.Fatal(err)
-  }
+	config, err := ConfigFromPathOrDefault("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  if err := config.MakeDirs(); err != nil {
-    log.Fatal(err)
-  }
+	if err := config.MakeDirs(); err != nil {
+		log.Fatal(err)
+	}
 
-  configInstance = &config
+	configInstance = &config
 
-  moduleCache = make(map[string][]byte)
+	moduleCache = make(map[string][]byte)
 
-  addr := config.Addr + ":" + config.Port
-  db := SetupDatabase(configInstance.Db)
+	addr := config.Addr + ":" + config.Port
+	db := SetupDatabase(configInstance.Db)
 
-  conn := NewConn(db)
-  server := NewServer(conn)
+	conn := NewConn(db)
+	server := NewServer(conn)
 
-  r := mux.NewRouter()
+	r := mux.NewRouter()
 
-  // send function JSON configuration using id from upload
-  r.HandleFunc("/upload", server.upload).Methods("POST")
+	// send function JSON configuration using id from upload
+	r.HandleFunc("/upload", server.upload).Methods("POST")
 
-  // list functions
-  r.HandleFunc("/functions", server.listFunctions).Methods("GET")
+	// list functions
+	r.HandleFunc("/functions", server.listFunctions).Methods("GET")
 
-  // call function
-  r.HandleFunc("/wasm/{id}", server.handler)
+	// call function
+	r.HandleFunc("/wasm/{id}", server.handler)
 
-  httpServer := http.Server{
-    Addr:        addr,
-    ConnContext: SaveConnection,
-    Handler:     r,
-  }
+	httpServer := http.Server{
+		Addr:        addr,
+		ConnContext: SaveConnection,
+		Handler:     r,
+	}
 
-  log.Println("Started Server on", addr)
-  err = httpServer.ListenAndServe()
-  log.Fatal(err)
+	log.Println("Started Server on", addr)
+	err = httpServer.ListenAndServe()
+	log.Fatal(err)
 }
--- server.go
+++ server.go
@@ -4,228 +4,244 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-  "io"
-  "log"
-  "net/http"
-  "os"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"path"
 )
 
+var instances map[string]*WasmInstance
+
 type Server struct {
-  conn Conn
+	conn Conn
 }
 
 func NewServer(conn Conn) Server {
-  return Server{
-    conn,
-  }
+	instances = make(map[string]*WasmInstance)
+	return Server{
+		conn,
+	}
 }
 
 func (s *Server) upload(w http.ResponseWriter, req *http.Request) {
-  err := req.ParseMultipartForm(32 << 20)
-  if err != nil {
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	err := req.ParseMultipartForm(32 << 20)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  tx, err := s.conn.Start()
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    return
-  }
+	tx, err := s.conn.Start()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-  var config FunctionConfig
-  data := req.MultipartForm.Value["config"]
-  err = config.parse(data[0])
+	var config FunctionConfig
+	data := req.MultipartForm.Value["config"]
+	err = config.parse(data[0])
 
-  if err != nil {
-    tx.Rollback()
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	if err != nil {
+		tx.Rollback()
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  id, err := tx.AddFunction(config.Name)
-  if err != nil {
-    tx.Rollback()
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	id, err := tx.AddFunction(config.Name)
+	if err != nil {
+		tx.Rollback()
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  for _, name := range config.Folders {
-    if err := tx.AddFolder(name); err != nil {
-      tx.Rollback()
-      w.WriteHeader(http.StatusInternalServerError)
-      w.Write([]byte(err.Error()))
-      return
-    }
-  }
+	for _, name := range config.Folders {
+		if err := tx.AddFolder(name); err != nil {
+			tx.Rollback()
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+	}
 
-  blob := config.blob()
-  err = tx.AddConfigBlob(id, config.Timeout, &blob)
-  if err != nil {
-    tx.Rollback()
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	blob := config.blob()
+	err = tx.AddConfigBlob(id, config.Timeout, &blob)
+	if err != nil {
+		tx.Rollback()
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  for _, method := range config.Methods {
-    err = tx.AddMethod(id, method)
-    if err != nil {
-      tx.Rollback()
-      w.WriteHeader(http.StatusInternalServerError)
-      w.Write([]byte(err.Error()))
-      return
-    }
-  }
+	for _, method := range config.Methods {
+		err = tx.AddMethod(id, method)
+		if err != nil {
+			tx.Rollback()
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+	}
 
-  file, _, err := req.FormFile("module")
-  if err != nil {
-    tx.Rollback()
-    w.WriteHeader(500)
-    w.Write([]byte(err.Error()))
-    return
-  }
-  defer file.Close()
+	file, _, err := req.FormFile("module")
+	if err != nil {
+		tx.Rollback()
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer file.Close()
 
-  destPath := path.Join(configInstance.ModulesDir(), fmt.Sprintf("%d.wasm", id))
+	destPath := path.Join(configInstance.ModulesDir(), fmt.Sprintf("%d.wasm", id))
 
-  dst, err := os.Create(destPath)
-  if err != nil {
-    tx.Rollback()
-    w.WriteHeader(500)
-    return
-  }
-  defer dst.Close()
+	dst, err := os.Create(destPath)
+	if err != nil {
+		tx.Rollback()
+		w.WriteHeader(500)
+		return
+	}
+	defer dst.Close()
 
-  if _, err := io.Copy(dst, file); err != nil {
-    tx.Rollback()
-    w.WriteHeader(500)
-    return
-  }
+	if _, err := io.Copy(dst, file); err != nil {
+		tx.Rollback()
+		w.WriteHeader(500)
+		return
+	}
 
-  w.WriteHeader(200)
-  tx.Commit()
+	w.WriteHeader(200)
+	tx.Commit()
 }
 
 func (s *Server) listFunctions(w http.ResponseWriter, req *http.Request) {
-  tx, err := s.conn.Start()
-  if err != nil {
-    w.WriteHeader(500)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	tx, err := s.conn.Start()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  collection, err := tx.GetFunctions()
-  if err != nil {
-    w.WriteHeader(500)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	collection, err := tx.GetFunctions()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(collection)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(collection)
 }
 
 func (s *Server) handler(w http.ResponseWriter, req *http.Request) {
-  vars := mux.Vars(req)
-  id := vars["id"]
-  fmt.Println("handler called:", id)
+	vars := mux.Vars(req)
+	id := vars["id"]
+	fmt.Println("handler called:", id)
 
-  tx, err := s.conn.Start()
-  if err != nil {
-    log.Println(err)
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	tx, err := s.conn.Start()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  valid, err := tx.CheckMethod(id, req.Method)
-  if err != nil || !valid {
-    tx.Rollback()
-    log.Println(err)
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write([]byte("Invalid Function ID"))
-    return
-  }
+	valid, err := tx.CheckMethod(id, req.Method)
+	if err != nil || !valid {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid Function ID"))
+		return
+	}
 
-  config, err := tx.GetConfig(id)
-  if err != nil {
-    tx.Rollback()
-    log.Println(err)
-    w.WriteHeader(http.StatusBadRequest)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	config, err := tx.GetConfig(id)
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  folders, err := tx.GetFolders(config.Folders)
-  if err != nil {
-    tx.Rollback()
-    log.Println(err)
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	folders, err := tx.GetFolders(config.Folders)
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
+	if instances[id] == nil {
+		container, err := NewWasmInstance(id, &config, folders, req)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(500)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
 
-  container, err := NewWasmInstance(id, &config, folders, req)
-  if err != nil {
-    log.Println(err)
-    w.WriteHeader(500)
-    json.NewEncoder(w).Encode(err.Error())
-    return
-  }
+		instances[id] = container
+	}
 
-  // timestamp := time.Now().UTC()
-  timeout := minInt64(config.timeout, 15 * 60 * 1000)
+	// container, err := NewWasmInstance(id, &config, folders, req)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	w.WriteHeader(500)
+	// 	json.NewEncoder(w).Encode(err.Error())
+	// 	return
+	// }
 
-  output, err := container.runModule(timeout)
-  if output == "" && err == nil {
-    log.Println(err)
-    w.WriteHeader(http.StatusRequestTimeout)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	container := instances[id]
 
-  // elapsed := maxInt64(1, container.Elapsed.Milliseconds())
+	// timestamp := time.Now().UTC()
+	timeout := minInt64(config.timeout, 15*60*1000)
 
-  log.Println("execution time (micro):", container.Elapsed.Microseconds())
-  err = tx.Commit()
+	output, err := container.runModule(timeout)
+	if output == "" && err == nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusRequestTimeout)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-  /*
-  err = tx.AddMetric(id, timestamp, elapsed)
-  if err != nil {
-    log.Println(err)
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte(err.Error()))
-    return
-  }
-  */
+	// elapsed := maxInt64(1, container.Elapsed.Milliseconds())
 
-  if err != nil {
-    log.Println(err)
-    w.WriteHeader(500)
-    w.Write([]byte(err.Error()))
-    return
-  }
+	log.Println("execution time (micro):", container.Elapsed.Microseconds())
+	err = tx.Commit()
 
-  conn := GetConn(req)
-  conn.Write([]byte(output))
+	/*
+	  err = tx.AddMetric(id, timestamp, elapsed)
+	  if err != nil {
+	    log.Println(err)
+	    w.WriteHeader(http.StatusInternalServerError)
+	    w.Write([]byte(err.Error()))
+	    return
+	  }
+	*/
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	conn := GetConn(req)
+	conn.Write([]byte(output))
 }
 
 func minInt64(a, b int64) int64 {
-  if a < b {
-    return a
-  }
-  return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func maxInt64(a, b int64) int64 {
-  if a > b {
-    return a
-  }
-  return b
+	if a > b {
+		return a
+	}
+	return b
 }
